d486ea68bf607ecadd676fcc767e632f35d1b5f8
ViktoriiaBilaniuk <vika.bilaniuk@gmail.com>
2019-05-25 22:56:57 +0300

    Chat component with hardcoded data

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { HomeRoutes } from './home.routing';
 import { HomeComponent } from './home.component';
 import { TeamsComponent } from './teams/teams.component';
 import { TeamLinkComponent } from './teams/team-link/team-link.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   imports: [CommonModule, HomeRoutes, TranslateModule],
-  declarations: [HomeComponent, TeamsComponent, TeamLinkComponent],
+  declarations: [HomeComponent, TeamsComponent, TeamLinkComponent, ChatComponent],
 })
 export class HomeModule {}
